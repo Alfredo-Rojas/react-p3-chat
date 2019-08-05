82b915a023889d4e34e5015e4d448cc8c0f54b72
Alfredo-Rojas <alfredot84@gmail.com>
2019-08-05 14:39:10 -0400

    Messege component created

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,29 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import Message from "./Message";
 
-const DUMMY_DATA = [
-  {
-    senderId: "perborgen",
-    text: "Hey, how is it going?"
-  },
-  {
-    senderId: "janedoe",
-    text: "Great! How about you?"
-  },
-  {
-    senderId: "perborgen",
-    text: "Good to hear! I am great as well"
+class MessageList extends React.Component {
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this);
+    this.shouldScrollToBottom =
+      node.scrollTop + node.clientHeight + 100 >= node.scrollHeight;
   }
-];
 
-class MessageList extends Component {
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      const node = ReactDOM.findDOMNode(this);
+      node.scrollTop = node.scrollHeight;
+    }
+  }
+
   render() {
+    if (!this.props.roomId) {
+      return (
+        <div className="message-list">
+          <div className="join-room">Join a room! &rarr;</div>
+        </div>
+      );
+    }
     return (
       <div className="message-list">
-        {DUMMY_DATA.map((message, index) => {
+        {this.props.messages.map((message, index) => {
           return (
-            <div key={index} className="message">
-              <div>{message.senderId}</div>,<div>{message.text}</div>;
-            </div>
+            <Message
+              key={message.id}
+              username={message.senderId}
+              text={message.text}
+            />
           );
         })}
       </div>
